998c36ceca692af8e115db0fb59aba649555a910
Venkatesh Badarala <21PA1A1206@vishnu.edu.in>
2024-10-26 13:11:04 +0530

    remove login

--- app/components/Forms/LoginForm.jsx
+++ app/components/Forms/LoginForm.jsx
@@ -104,7 +104,7 @@ const LoginForm = () => {
               <div className="w-full h-px bg-gray-300" />
             </div>
 
-            <LoginProvider />
+            {/* <LoginProvider /> */}
 
            
           </div>
